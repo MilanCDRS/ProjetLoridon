63dc07e46afbb7b9b71df39a6ebf9385eac6c065
Milan <milan.cdrs@gmail.com>
2023-11-07 11:06:41 +0100

    Correction Departement

--- classes/Departement.php
+++ classes/Departement.php
@@ -1,32 +1,31 @@
 <?php
-    class Departement extends Region{
-        private $numero;
-        private $code;
-        private $nom;
-        private $blason;
+class Departement{
+    private $numero;
+    private $code;
+    private $nom;
+    private $blason;
 
-        public function __construct($code, $libelle, $numero, $code, $nom, $blason){
-            parent:: __construct($code, $libelle);
-            $this->numero=$numero;
-            $this->code=$code;
-            $this->nom=$nom;
-            $this->blason=$blason;
-        }
-
-        public function getNumero(){
-            return $this->numero;
-        }
-
-        public function getCode(){
-            return $this->code;
-        }
-
-        public function getNom(){
-            return $this->nom;
-        }
-
-        public function getBlason(){
-            return $this->blason;
-        }
+    public function __construct($numero, $code, $nom, $blason){
+        $this->numero=$numero;
+        $this->code= $code;
+        $this->nom=$nom;
+        $this->blason=$blason;
     }
+
+    public function getNumero(){
+        return $this->numero;
+    }
+
+    public function getCode(){
+        return $this->code;
+    }
+
+    public function getNom(){
+        return $this->nom;
+    }
+
+    public function getBlason(){
+        return $this->blason;
+    }
+}
 ?>
