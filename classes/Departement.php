cfbd6367d560c7bda7197250969b567645957538
Corail <146718250+CoCorail@users.noreply.github.com>
2023-11-07 11:22:29 +0100

    Corrections Département (Get/Set)

--- classes/Departement.php
+++ classes/Departement.php
@@ -1,10 +1,12 @@
 <?php
 class Departement{
+    //Variables 
     private $numero;
     private $code;
     private $nom;
     private $blason;
 
+    //Constructor
     public function __construct($numero, $code, $nom, $blason){
         $this->numero=$numero;
         $this->code= $code;
@@ -12,20 +14,25 @@ class Departement{
         $this->blason=$blason;
     }
 
-    public function getNumero(){
-        return $this->numero;
+    //Getters
+    public function __get($propriete) {
+        switch ($propriete) {
+            case "numero" : return $this->_numero; break;
+            case "code" : return $this->_code; break;
+            case "nom" : return $this->_nom; break;
+            case "blason" : return $this->_blason; break;
+        }
     }
 
-    public function getCode(){
-        return $this->code;
+    //Setters
+    public function __set($propriete, $value) {
+        switch ($propriete) {
+            case "numero" : $this->_numero = $value; break;
+            case "code" : return $this->_code = $value; break;
+            case "nom" : return $this->_nom = $value; break;
+            case "blason" : return $this->_blason = $value; break;
+        }
     }
-
-    public function getNom(){
-        return $this->nom;
-    }
-
-    public function getBlason(){
-        return $this->blason;
-    }
+    
 }
 ?>
