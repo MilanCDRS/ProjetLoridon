c8c7da1975d52a8f19989e1590dd2fbf27a322f9
unknown <milan.cdrs@gmail.com>
2023-11-15 15:16:17 +0100

    Click sur carte et recherhce par region

--- view/specialites.php
+++ view/specialites.php
@@ -1,5 +1,14 @@
 <H1>Liste des Spécialités<H1>
+    
 <?php 
+// recupere 
+if(isset($_GET['r']))
+{
+    $idReg = $_GET['r']; 
+    $reg = GetRegionByCode($idReg);
+    $lesSpecialites = GetSpecialitesByRegion($reg);
+}
+
 foreach($lesSpecialites as $spe)
 {
 ?>
