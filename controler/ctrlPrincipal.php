1c19005728fb854972987892dc74b33a00347549
Milan <milan.cdrs@gmail.com>
2023-11-28 10:03:34 +0100

    ligne inutile

--- controler/ctrlPrincipal.php
+++ controler/ctrlPrincipal.php
@@ -9,7 +9,6 @@ include_once "$racine/classes/Region.php";
 include_once "$racine/classes/Specialite.php";
 include_once "$racine/classes/Type.php";
 include_once "$racine/classes/User.php";
-include_once "$racine/authBDD.php";
 
 // en fct de l'action, revoie sur les autres controlleurs
 function ctrlPrincipal($action){
