0c126c849f4039d739a5ce6f71f69199cbd001a5
Simon Poinceaux <spoinceaux.sio@gmail.com>
2023-12-12 15:33:23 +0100

    Modif BDD Note

--- sql/scriptNote.sql
+++ sql/scriptNote.sql
@@ -1,13 +1,1 @@
-DROP TABLE IF EXISTS Note;
-CREATE TABLE IF NOT EXISTS Note(
-	identUser INT(10) NOT NULL,
-    idSpecialite INT(10) NOT NULL,
-    note INT(1)  NOT NULL,
-    PRIMARY KEY(identUser,idSpecialite),
-    FOREIGN KEY(identUser) REFERENCES User(ident),
-    FOREIGN KEY(idSpecialite) REFERENCES Specialite(id)
-)Engine=InnoDB;
 
-select * from note;
-
-select * from user;
