2a8751f17aa52cce0d69aeb62ed4f0504522b968
CDRS <68082649+MilanCDRS@users.noreply.github.com>
2023-12-18 00:03:47 +0100

    Update/Correctif login.php

--- controler/login.php
+++ controler/login.php
@@ -4,6 +4,11 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
 }
 include_once "$racine/model/authBDD.php";
 
+if(isLoggedOn()){
+    logout();
+    header("Location: index.php");
+}
+
 // creation du menu burger
 //$menuBurger = array();
 //$menuBurger[] = Array("url"=>"./?action=connexion","label"=>"Connexion");
@@ -13,31 +18,26 @@ include_once "$racine/model/authBDD.php";
 if (isset($_POST["mail"]) && isset($_POST["mdp"])){
     $mailU=$_POST["mail"];
     $mdpU=$_POST["mdp"];
-    if(isLoggedOn()){
-        header("Location: index.php");
-    }
-    else{ // l'utilisateur n'est pas connecté, on affiche le formulaire de connexion
+    
+    // l'utilisateur n'est pas connecté, on affiche le formulaire de connexion
         // appel du script de vue 
         $titre = "authentification";
         include "$racine/view/header.php";
         include "$racine/view/viewAuth.php";
         include "$racine/view/footer.php";
-    }
+    
 }
 else
 {
     $mailU="";
     $mdpU="";
-    if(isLoggedOn()){
-        logout();
-    }
-    else{ // l'utilisateur n'est pas connecté, on affiche le formulaire de connexion
+    // l'utilisateur n'est pas connecté, on affiche le formulaire de connexion
         // appel du script de vue 
         $titre = "authentification";
         include "$racine/view/header.php";
         include "$racine/view/viewAuth.php";
         include "$racine/view/footer.php";
-    }
+    
 }
 
 // appel des fonctions permettant de recuperer les donnees utiles a l'affichage 
@@ -45,6 +45,9 @@ else
 
 // traitement si necessaire des donnees recuperees
 $check=login($mailU,$mdpU);
+if($check == 1){
+    header("Location: index.php");
+}
 /*
 if (isLoggedOn()){ // si l'utilisateur est connecté on redirige vers le controleur monProfil
     //include "$racine/controleur/monProfil.php";
