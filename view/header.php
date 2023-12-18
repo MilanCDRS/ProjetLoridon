388093b80e3fcac362891c310ccb38a7779ce448
ndauga <ndauga.sio@gmail.com>
2023-12-18 16:19:15 +0100

    Merge NDHome/Corail

--- view/header.php
+++ view/header.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>BOUFFE DE NOS TERROIRS</title>
+        <title>La Bouffe de Là-bas</title>
         <style type="text/css">
             @import url("css/style.css");
             @import url("css/spe.css");
@@ -15,15 +15,69 @@
     </head>
     <body>
         <header>
-            <img id=logo src="images/Visuals/logo.png">
-            <img class="side left" src="images/Visuals/HeaderSide.png">
-            <img class="side right" src="images/Visuals/HeaderSide.png">
-            <a class=accueil href="./?action=default">Acceuil</a>           
-            <?php 
-            if(!isLoggedOnAsAdmin())
-                echo '<a class=crud href="./?action=crud">CRUD</a>';
-            ?>
-            <a class=cnx href="./?action=login">Se Connecter</a>
+            <div class="bannerinside">
+                <img id=logo src="images/Visuals/logo.png">
+                <a class=accueil href="./?action=default">Accueil</a>           
+                <?php 
+                if(isLoggedOnAsAdmin())
+                    //CRUD s'affiche que si un admin est connecté
+                    echo '<a class=crud href="./?action=crud">CRUD</a>';
+                ?>
+                <a class=cnx href="./?action=login">Se Connecter</a>
+            </div>
         </header>
         <div id=header></div>
+        <section class="carousel" aria-label="Gallery">
+        <ol class="carousel__viewport">
+            <li id="carousel__slide1"
+                tabindex="0"
+                class="carousel__slide">
+            <div class="carousel__snapper">
+                <a href="#carousel__slide4">
+                <a href="#carousel__slide2">
+            </div>
+            </li>
+            <li id="carousel__slide2"
+                tabindex="0"
+                class="carousel__slide">
+            <div class="carousel__snapper"></div>
+            <a href="#carousel__slide1">
+            <a href="#carousel__slide3">
+            </li>
+            <li id="carousel__slide3"
+                tabindex="0"
+                class="carousel__slide">
+            <div class="carousel__snapper"></div>
+            <a href="#carousel__slide2">
+            <a href="#carousel__slide4">
+            </li>
+            <li id="carousel__slide4"
+                tabindex="0"
+                class="carousel__slide">
+            <div class="carousel__snapper"></div>
+            <a href="#carousel__slide3">
+            <a href="#carousel__slide1">
+            </li>
+        </ol>
+            <aside class="carousel__navigation">
+                <ol class="carousel__navigation-list">
+                <li class="carousel__navigation-item">
+                    <a href="#carousel__slide1"
+                    class="carousel__navigation-button">Go to slide 1</a>
+                </li>
+                <li class="carousel__navigation-item">
+                    <a href="#carousel__slide2"
+                    class="carousel__navigation-button">Go to slide 2</a>
+                </li>
+                <li class="carousel__navigation-item">
+                    <a href="#carousel__slide3"
+                    class="carousel__navigation-button">Go to slide 3</a>
+                </li>
+                <li class="carousel__navigation-item">
+                    <a href="#carousel__slide4"
+                    class="carousel__navigation-button">Go to slide 4</a>
+                </li>
+                </ol>
+            </aside>
+    </section>
    
